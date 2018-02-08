66a549af0fa22a2ff2db33c361e3d78f7136e882
Malakot <36247392+Malakot@users.noreply.github.com>
2018-02-08 04:10:14 +0330

    Update SeenHack.sh

--- SeenHack.sh
+++ SeenHack.sh
@@ -3,11 +3,9 @@ clear
 Malakot () {
 read -p "please enter link address : " link
 read -p "Please enter the script reboot time! : " restart
-read -p "plaese enter addres folder tor! : " tor
 while [ "$link" == "$link" ];do
-cd $tor
 sleep 2
-./$@ https://t.me/$link
+env ./start-tor-browser.desktop https://t.me/$link
 sleep $restart
 killall firefox
 sleep 2
@@ -41,6 +39,5 @@ echo "
                                          |					  |
                                          |					  |
                                          ||--[]---[]---[]---[]---[]------[]--[]-| |
-
 "
 Malakot
